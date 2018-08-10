8bf9cda89ebebd77d2756c2a32d1505224b3deb1
WolfDan <5377526+WolfDan@users.noreply.github.com>
2018-08-10 17:40:45 -0500

    This commit implements the following:
    - Updated Readme roadmap
    - Renaming Vertex to Edge since is the correct term
    - Implementing Edge set and get
    - Fixing test cases
    - Cleaning up the code

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ This is the database we use at [Okami](https://www.okami.io)!
 Is powered by [FoundationDB](https://www.foundationdb.org) and is mean to be graph database for all our needs, 
 check the source for more info!
 
-*The database code still in development, be careful using it*
+***This project is a work in progress, be careful using it***
 
 # RoadMap
 
-- Vertex implementation
-    - set data
+- Update support
+
+- Delete support
+
+- Edge implementation
     - get data
 
 - A better error handling: right now the errors are covered, but returning None making it difficult to know 
@@ -19,6 +22,10 @@ why is the problem
 - `where` query support 
     - Datetime sorting and query
 
-- SDL parsing
+- SDL (GraphQL) parsing
 
 - Network Protocol implementation
+    - GraphQL
+    - Gremlin
+
+- More tests
--- test.py
+++ test.py
@@ -27,7 +27,7 @@ class TestingNodeBasicProperties(unittest.TestCase):
         self.assertEqual(self.user_node.get_all(self.db, uid),
                          {b'email': b'test@test.com', b'name': b'sif', b'password': b'super_strong',
                           b'uid': b'\x01\x00\x00\x00\x00\x00\x00\x00'})
-        self.assertEqual(self.user_node.get_vertex_value(self.db, uid, 'name'), b'sif')
+        self.assertEqual(self.user_node.get_edge_value(self.db, uid, 'name'), b'sif')
         self.assertEqual(self.user_node.get_by(self.db, 'name', 'sif'), [
             {b'email': b'test@test.com', b'name': b'sif', b'password': b'super_strong',
              b'uid': b'\x01\x00\x00\x00\x00\x00\x00\x00'}])
@@ -64,7 +64,7 @@ class TestingNodeBasicProperties(unittest.TestCase):
         self.assertEqual(self.user_node.get_all(self.db, uid3),
                          {b'email': b'test@test.comm', b'name': b'sif2', b'password': b'123456',
                           b'uid': b'\x02\x00\x00\x00\x00\x00\x00\x00'})
-        self.assertEqual(self.user_node.get_vertex_value(self.db, uid3, 'name'), b'sif2')
+        self.assertEqual(self.user_node.get_edge_value(self.db, uid3, 'name'), b'sif2')
         self.assertEqual(self.user_node.get_by(self.db, 'name', 'sif2'), [
             {b'email': b'test@test.comm', b'name': b'sif2', b'password': b'123456',
              b'uid': b'\x02\x00\x00\x00\x00\x00\x00\x00'}])
@@ -100,18 +100,24 @@ class TestingNodeRelationships(unittest.TestCase):
             'password': '123456'
         })
         msg_uid = self.msg_node.set(self.db, {
-            'from_user': uid,
-            'to_user': uid3,
+            'from_user': {
+                'edge': None,
+                'data': uid
+            },
+            'to_user': {
+                'edge': None,
+                'data': uid3
+            },
             'value': 'testing a message from nodes'
         })
 
-        self.assertEqual(self.msg_node.get_all(self.db, msg_uid), {b'from_user': b'\x01\x00\x00\x00\x00\x00\x00\x00',
-                                                                   b'to_user': b'\x02\x00\x00\x00\x00\x00\x00\x00',
+        self.assertEqual(self.msg_node.get_all(self.db, msg_uid), {b'from_user': [b'\x01\x00\x00\x00\x00\x00\x00\x00'],
+                                                                   b'to_user': [b'\x02\x00\x00\x00\x00\x00\x00\x00'],
                                                                    b'uid': b'\x03\x00\x00\x00\x00\x00\x00\x00',
                                                                    b'value': b'testing a message from nodes'})
-        self.assertEqual(self.msg_node.get_vertex_value(self.db, msg_uid, 'value'), b'testing a message from nodes')
+        self.assertEqual(self.msg_node.get_edge_value(self.db, msg_uid, 'value'), b'testing a message from nodes')
         self.assertEqual(self.msg_node.get_by(self.db, 'value', 'testing a message from nodes'), [
-            {b'from_user': b'\x01\x00\x00\x00\x00\x00\x00\x00', b'to_user': b'\x02\x00\x00\x00\x00\x00\x00\x00',
+            {b'from_user': [b'\x01\x00\x00\x00\x00\x00\x00\x00'], b'to_user': [b'\x02\x00\x00\x00\x00\x00\x00\x00'],
              b'uid': b'\x03\x00\x00\x00\x00\x00\x00\x00', b'value': b'testing a message from nodes'}])
 
         a = {
@@ -129,8 +135,8 @@ class TestingNodeRelationships(unittest.TestCase):
 
         self.assertEqual(self.msg_node.get(self.db, msg_uid, a),
                          {'uid': b'\x03\x00\x00\x00\x00\x00\x00\x00', 'value': b'testing a message from nodes',
-                          'to_user': {'name': b'sif2', 'password': b'123456'},
-                          'from_user': {'name': b'sif', 'email': b'test@test.com'}})
+                          'to_user': [{'name': b'sif2', 'password': b'123456'}],
+                          'from_user': [{'name': b'sif', 'email': b'test@test.com'}]})
 
 
 if __name__ == '__main__':
